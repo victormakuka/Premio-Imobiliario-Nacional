62d1528695bfe972593aa99a10d72d29b20471c9
Victor Makuka <149114463+victormakuka@users.noreply.github.com>
2025-10-21 23:16:30 +0100

    Updates

---
+++ ParceirosPNI.vue
@@ -0,0 +1,10 @@
+<template>
+     <section class="border-t border-b border-gray-200 p-4 lg:py-20">
+    <div class="max-w-7xl mx-auto pt-12">
+      <h1 class="font-montserrat font-bold text-azul text-2xl">Parceiros</h1>
+      <h2 class="font-open text-sm md:text-base text-gray-900 pb-8">
+        Descubra os profissionais e empresas reconhecidos pela excelência.
+      </h2>
+      </div>
+    </section>
+</template>
